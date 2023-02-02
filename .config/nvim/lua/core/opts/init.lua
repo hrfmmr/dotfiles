31ccccf16d111c95bdaaac4ca4e90ae03a44d9e7
hrfmmr <i@hrfmmr.com>
2023-02-02 23:54:26 +0900

    fixup! [wip] Use lua for nvim configuration

--- .config/nvim/lua/core/opts/init.lua
+++ .config/nvim/lua/core/opts/init.lua
@@ -1,2 +1,4 @@
 require("core.opts.set")
 require("core.opts.cmd")
+require("core.opts.keymap")
+require("core.opts.term")
---
+++ .config/nvim/lua/core/opts/keymap.lua
@@ -0,0 +1,56 @@
+-- global
+vim.g.mapleader = ","
+vim.keymap.set("i", "<C-c>", "<ESC>")
+vim.keymap.set("", "s", "<Nop>")
+vim.keymap.set("n", "<Leader>R", function()
+	require("utils").reloadConfig()
+end, { desc = "Reload nvim config" })
+
+-- insert new line
+vim.keymap.set("n", "<CR>", "i<CR><ESC>")
+-- Switch tab
+vim.keymap.set("n", "gc", "gt")
+vim.keymap.set("n", "gr", "gT")
+-- Move tab
+vim.keymap.set("n", "gC", ":+tabm<CR>", { silent = true })
+vim.keymap.set("n", "gC", ":-tabm<CR>", { silent = true })
+-- Emacs key bind for moving cursole while INSERT
+vim.keymap.set("i", "<c-n>", "<down>")
+vim.keymap.set("i", "<c-p>", "<up>")
+vim.keymap.set("i", "<c-b>", "<left>")
+vim.keymap.set("i", "<c-f>", "<right>")
+vim.keymap.set("i", "<c-a>", "<Esc>_i")
+vim.keymap.set("i", "<c-e>", "<Esc>$a")
+-- Split window
+vim.keymap.set("n", "<Space>V", ":new<CR>", { silent = true })
+vim.keymap.set("n", "<Space>H", ":vne<CR>", { silent = true })
+-- Switch window
+vim.keymap.set("n", "<c-j>", "<c-w>j")
+vim.keymap.set("n", "<c-k>", "<c-w>k")
+vim.keymap.set("n", "<c-l>", "<c-w>l")
+vim.keymap.set("n", "<c-h>", "<c-w>h")
+-- Paging
+vim.keymap.set("n", "<Space>j", "<c-f>")
+vim.keymap.set("n", "<Space>k", "<c-b>")
+-- File write/quit/update
+vim.keymap.set("n", "<Space>w", ":w<CR>", { silent = true })
+vim.keymap.set("n", "<Space>q", ":q<CR>", { silent = true })
+vim.keymap.set("n", "<Space>Q", ":bd!<CR>", { silent = true })
+vim.keymap.set("n", "<Space>e", ":e!<CR>", { silent = true })
+-- Tab Handling
+vim.keymap.set("n", "<leader>te", ":<C-u>tabedit<CR>", { silent = true })
+vim.keymap.set("n", "<leader>tc", ":<C-u>tabclose<CR>", { silent = true })
+vim.keymap.set("n", "<leader>ts", ":tab<space>split<CR>", { silent = true })
+-- Search visual selected
+vim.keymap.set("v", "//", 'y/<C-R>"<CR>')
+-- quickfix
+vim.keymap.set("n", "[q", ":cprevious<CR>", { silent = true })
+vim.keymap.set("n", "]q", ":cnext<CR>", { silent = true })
+vim.keymap.set("n", "[Q", ":<C-u>cfirst<CR>", { silent = true })
+vim.keymap.set("n", "]Q", ":<C-u>clast<CR>", { silent = true })
+vim.keymap.set("n", "<leader>qo", ":copen")
+vim.keymap.set("n", "<leader>qc", ":cclose")
+-- term
+vim.keymap.set("n", "<C-t>", ":term<CR>", { silent = true })
+vim.keymap.set("t", "<C-[>", "<C-\\><C-n>")
+vim.keymap.set("t", "<C-q>", "<ESC>")
