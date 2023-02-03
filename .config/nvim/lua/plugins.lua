df091eff5bcaf98ae9291e2118f12c64e7785546
hrfmmr <i@hrfmmr.com>
2023-02-03 21:41:01 +0900

    fixup! [wip] Use lua for nvim configuration

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -157,4 +157,47 @@ require("lazy").setup({
 		end,
 	},
 	-- }}}
+
+	-- git {{{
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.cmd([[
+              nnoremap [fugitive] <Nop>
+              nmap     <Leader>g   [fugitive]
+              nnoremap <silent> [fugitive]b :Git blame<CR>
+              nnoremap <silent> [fugitive]B :GBrowse<CR>
+              nnoremap <silent> [fugitive]s :tab Git<CR>
+              nnoremap <silent> [fugitive]w :Gwrite<CR>
+              nnoremap <silent> [fugitive]c :Gcommit<CR>
+              nnoremap <silent> [fugitive]d :Gdiffsplit<CR>
+              nnoremap <silent> [fugitive]r :tab Git! diff <CR>
+              nnoremap <silent> [fugitive]R :tab Git! diff --staged<CR>
+            ]])
+		end,
+	},
+	{ "tpope/vim-rhubarb", dependencies = {
+		"tpope/vim-fugitive",
+	} },
+	{ "tpope/vim-dispatch" },
+	{ "airblade/vim-gitgutter" },
+	{
+		"rbong/vim-flog",
+		config = function()
+			vim.cmd([[
+              nnoremap <silent> <Leader>gv :Flog<CR>
+              nnoremap <silent> <Leader>gV :Flogsplit -path=%<CR>
+              let g:flog_default_opts = {
+                \ 'max_count': 2000,
+                \ 'all': 1,
+                \ }
+              augroup FlogBindings
+                au!
+                au FileType floggraph nnoremap <buffer> <silent> <Tab> :<C-U>call flog#set_commit_mark_at_line('m', '.') \| call flog#run_command('vertical botright Gsplit %h:%p', 0, 0, 1)<CR>
+                au FileType floggraph nnoremap <buffer> <silent> df :<C-U>call flog#run_command("vertical botright Gsplit %(h'm):%p \| Gdiffsplit %h", 0, 0, 1)<CR>
+              augroup END
+            ]])
+		end,
+	},
+	-- }}}
 })
