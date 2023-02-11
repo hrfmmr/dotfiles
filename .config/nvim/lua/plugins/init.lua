d1404bbcde378d07deeb9b17854a27907957dcdd
hrfmmr <i@hrfmmr.com>
2023-02-11 23:37:54 +0900

    Fix NERDTree config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -401,6 +401,10 @@ require("lazy").setup({
               nnoremap <silent> [nerdtree]c :NERDTreeFind<CR>
               nnoremap <silent> [nerdtree]n :NERDTreeToggle<CR>
               nnoremap <silent> [nerdtree]r :NERDTree .<CR>
+
+              let g:NERDTreeShowHidden=1
+              let g:NERDTreeChDirMode=2
+              let g:NERDTreeIgnore = ['\.pyc$', '__pycache__']
             ]])
 		end,
 	},
