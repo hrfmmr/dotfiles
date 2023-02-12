d607d63137ab261cdb337a21f86c64d519d3cf86
hrfmmr <i@hrfmmr.com>
2023-02-12 15:13:06 +0900

    Add trouble.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -829,6 +829,41 @@ require("lazy").setup({
 	-- },
 	-- }}}
 
+	-- Diagnostics {{{
+	{
+		"folke/trouble.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("trouble").setup({
+				height = 20,
+			})
+			vim.keymap.set("n", "<leader>dd", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
+			vim.keymap.set(
+				"n",
+				"<leader>dw",
+				"<cmd>TroubleToggle workspace_diagnostics<cr>",
+				{ silent = true, noremap = true }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>db",
+				"<cmd>TroubleToggle document_diagnostics<cr>",
+				{ silent = true, noremap = true }
+			)
+			vim.keymap.set("n", "<leader>dl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
+			vim.keymap.set("n", "<leader>dq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
+			vim.keymap.set(
+				"n",
+				"<leader>dr",
+				"<cmd>TroubleToggle lsp_references<cr>",
+				{ silent = true, noremap = true }
+			)
+		end,
+	},
+	-- }}}
+
 	-- Runner {{{
 	{
 		"Shougo/vimproc.vim",
