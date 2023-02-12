1b7b9795f964856f51757c4310cfb2fd39d96664
hrfmmr <i@hrfmmr.com>
2023-02-12 22:37:08 +0900

    Add diffview.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -459,6 +459,25 @@ require("lazy").setup({
             ]])
 		end,
 	},
+	{
+		"sindrets/diffview.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("diffview").setup({
+				view = {
+					merge_tool = {
+						layout = "diff3_mixed",
+					},
+				},
+			})
+			vim.keymap.set("n", "<Leader>gM", "<cmd>DiffviewOpen<cr>", { silent = true })
+			vim.keymap.set("n", "<Leader>gc", "<cmd>DiffviewClose<cr>", { silent = true })
+			vim.keymap.set("n", "<Leader>gH", "<cmd>DiffviewFileHistory<cr>", { silent = true })
+		end,
+	},
 	-- }}}
 
 	-- Language & Syntax {{{
