33ce9a5b2b826caf5be703a8e073a955541ab883
hrfmmr <i@hrfmmr.com>
2023-02-12 14:33:55 +0900

    Add jq utility command

--- .config/nvim/lua/core/opts/cmd.lua
+++ .config/nvim/lua/core/opts/cmd.lua
@@ -11,3 +11,15 @@ vim.api.nvim_create_user_command("Scriptnames", function()
 		false
 	)
 end, { desc = "List all loaded vim/lua script names on new buffer" })
+
+vim.cmd([[
+function! s:Jq(...)
+  if 0 == a:0
+    let l:arg = "."
+  else
+    let l:arg = a:1
+  endif
+  execute "%! jq \"" . l:arg . "\""
+endfunction
+command! -nargs=? Jq call s:Jq(<f-args>)
+]])
