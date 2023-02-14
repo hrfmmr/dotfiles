4edf2d522bd60408a8d25a48d90e7a414a7e5304
hrfmmr <i@hrfmmr.com>
2023-02-14 23:13:12 +0900

    fixup! Refactor nvim plugins config

---
+++ .config/nvim/lua/plugins/config/nvim-cmp.lua
@@ -0,0 +1,69 @@
+return function()
+	local cmp = require("cmp")
+	vim.o.completeopt = "menuone,noselect"
+	local t = function(str)
+		return vim.api.nvim_replace_termcodes(str, true, true, true)
+	end
+	local check_back_space = function()
+		local col = vim.fn.col(".") - 1
+		return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
+	end
+	cmp.setup({
+		snippet = {
+			expand = function(args)
+				vim.fn["UltiSnips#Anon"](args.body)
+			end,
+		},
+		mapping = cmp.mapping.preset.insert({
+			["<CR>"] = cmp.mapping.confirm({ select = false }),
+			["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+			["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+			["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-e>"] = cmp.mapping.complete(),
+			["<C-q>"] = cmp.mapping.abort(),
+			-- expand snippet
+			["<C-y>"] = cmp.mapping(function(fallback)
+				if vim.fn["UltiSnips#CanExpandSnippet"]() == 1 then
+					vim.fn.feedkeys(t("<C-R>=UltiSnips#ExpandSnippet()<CR>"))
+				elseif check_back_space() then
+					vim.fn.feedkeys(t("<C-e>"), "n")
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+
+			-- cycle menu, jump through tabstop
+			["<Tab>"] = cmp.mapping(function(fallback)
+				if vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
+					vim.fn.feedkeys(t("<ESC>:call UltiSnips#JumpForwards()<CR>"))
+				elseif vim.fn.pumvisible() == 1 then
+					vim.fn.feedkeys(t("<C-n>"), "n")
+				elseif check_back_space() then
+					vim.fn.feedkeys(t("<tab>"), "n")
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+			["<S-Tab>"] = cmp.mapping(function(fallback)
+				if vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
+					return vim.fn.feedkeys(t("<C-R>=UltiSnips#JumpBackwards()<CR>"))
+				elseif vim.fn.pumvisible() == 1 then
+					vim.fn.feedkeys(t("<C-p>"), "n")
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+		}),
+		sources = cmp.config.sources({
+			{ name = "nvim_lsp" },
+			{ name = "nvim_lua" },
+			{ name = "buffer" },
+			{ name = "path" },
+			{ name = "emoji" },
+			{ name = "tmux", keyword_length = 2, option = { trigger_characters = {}, all_panes = true } },
+			{ name = "rg", keyword_length = 3 },
+			-- { name = "look", keyword_length = 2, option = { convert_case = true, loud = true } },
+		}),
+	})
+end
---
+++ .config/nvim/lua/plugins/config/operator-replace.lua
@@ -0,0 +1,3 @@
+return function()
+	vim.cmd("map R <Plug>(operator-replace)")
+end
