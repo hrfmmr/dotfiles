8193e3898ed4c6166ed9972b3b89e557b748398c
hrfmmr <i@hrfmmr.com>
2023-02-14 11:57:56 +0900

    Refactor nvim plugins config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -17,21 +17,7 @@ require("lazy").setup({
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha",
-				background = {
-					light = "latte",
-					dark = "mocha",
-				},
-				transparent_background = true,
-				term_colors = true,
-			})
-			vim.cmd([[colorscheme catppuccin]])
-			vim.cmd(
-				"highlight LineNr term=bold cterm=NONE ctermfg=DarkGrey ctermbg=NONE gui=NONE guifg=DarkGrey guibg=NONE"
-			)
-		end,
+		config = require("plugins.config.catppuccin"),
 	},
 	-- }}}
 
