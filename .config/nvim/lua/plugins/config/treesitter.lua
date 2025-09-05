e0ba0e4d9056ce8749214401eeddffe5696194e9
hrfmmr <i@hrfmmr.com>
2025-09-05 14:29:21 +0900

    Add json fold config

--- .config/nvim/lua/plugins/config/treesitter.lua
+++ .config/nvim/lua/plugins/config/treesitter.lua
@@ -5,4 +5,13 @@ return function()
 		},
 		ensure_installed = "all",
 	})
+
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "json",
+		callback = function()
+			vim.opt_local.foldmethod = "expr"
+			vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
+			vim.opt_local.foldlevel = 2
+		end,
+	})
 end
