942458781cd55597e06b199cd73c5706c91eb34d
hrfmmr <i@hrfmmr.com>
2025-10-31 13:19:06 +0900

    lsp: fix pyright pythonPath setup

--- .config/nvim/lua/plugins/lsp/settings/pyright.lua
+++ .config/nvim/lua/plugins/lsp/settings/pyright.lua
@@ -1,15 +1,17 @@
-local config = require("lspconfig.configs")
-local path = require("lspconfig.util").path
+local util = require("lspconfig.util")
+local path = util.path
 
-local function get_python_path(workspace)
+local function get_python_path(root_dir)
 	-- Use activated virtualenv.
 	if vim.env.VIRTUAL_ENV then
 		return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
 	end
 
+	root_dir = root_dir or vim.loop.cwd() or vim.fn.getcwd()
+
 	-- Find and use virtualenv in workspace directory.
 	for _, pattern in ipairs({ "*", ".*" }) do
-		local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
+		local match = vim.fn.glob(path.join(root_dir, pattern, "pyvenv.cfg"))
 		if match ~= "" then
 			return path.join(path.dirname(match), "bin", "python")
 		end
@@ -20,9 +22,9 @@ local function get_python_path(workspace)
 end
 
 return {
-	settings = {
-		python = {
-			pythonPath = get_python_path(config.root_dir),
-		},
-	},
+	on_new_config = function(new_config, root_dir)
+		new_config.settings = new_config.settings or {}
+		new_config.settings.python = new_config.settings.python or {}
+		new_config.settings.python.pythonPath = get_python_path(root_dir)
+	end,
 }
