7a09913db2719afa48169e6e1fec34cd8ad5278b
hrfmmr <i@hrfmmr.com>
2025-10-31 12:47:47 +0900

    lsp: wire up sourcekit via new API

--- .config/nvim/lua/plugins/config/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/config/nvim-lspconfig.lua
@@ -1,11 +1,6 @@
 return function()
-	local nvim_lsp = require("lspconfig")
-	local opts = {
-		on_attach = require("plugins.lsp.handler").on_attach,
-		capabilities = require("plugins.lsp.handler").capabilities,
-	}
-	-- sourcekit
-	nvim_lsp.sourcekit.setup(vim.tbl_deep_extend("force", opts, require("plugins.lsp.settings.sourcekit")))
+	vim.lsp.config("sourcekit", require("plugins.lsp.settings.sourcekit"))
+	vim.lsp.enable("sourcekit")
 
 	vim.keymap.set("n", "<C-g>il", ":LspInfo<CR>", { silent = true })
 end
