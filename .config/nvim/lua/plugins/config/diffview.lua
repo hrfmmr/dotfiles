2f63c8587fc5abbb639eb1d388ae2f1fed5e8de8
hrfmmr <i@hrfmmr.com>
2026-08-01 12:36:18 +0900

    feat(nvim): improve Diffview revision comparison mapping

--- .config/nvim/lua/plugins/config/diffview.lua
+++ .config/nvim/lua/plugins/config/diffview.lua
@@ -12,13 +12,18 @@ return {
 			},
 		})
 		vim.keymap.set("n", "<Leader>gM", "<cmd>DiffviewOpen<cr>", { silent = true })
-		vim.keymap.set("n", "<Leader>gr", function()
-			local hash = vim.fn.input("Base commit hash: ")
-			if hash ~= "" then
-				vim.cmd("DiffviewOpen " .. hash .. "..HEAD")
+		vim.keymap.set("n", "<Leader>gc", function()
+			local rev = vim.fn.input("Base rev (commit/branch/tag): ")
+			if rev ~= "" then
+				-- Pass through as-is if it already specifies a range (`..` / `...` / `^!`)
+				if rev:find("%.%.") or rev:find("%^!") then
+					vim.cmd("DiffviewOpen " .. rev)
+				else
+					vim.cmd("DiffviewOpen " .. rev .. "...HEAD")
+				end
 			end
 		end, { silent = true })
-		vim.keymap.set("n", "<Leader>gc", "<cmd>DiffviewClose<cr>", { silent = true })
+		vim.keymap.set("n", "<Leader>gC", "<cmd>DiffviewClose<cr>", { silent = true })
 		vim.keymap.set("n", "<Leader>gH", "<cmd>DiffviewFileHistory %<cr>", { silent = true })
 	end,
 }
